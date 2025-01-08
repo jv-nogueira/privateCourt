3f23b4c90296dca1ab224fdb8d92f60947c930a5
Joao Vitor Nogueira da Silva <joao.nogueira@digisystem.com.br>
2025-01-08 17:12:13 -0300

    Implementa icone

--- temp.js
+++ temp.js
@@ -1,3 +1,9 @@
 // Próximas etapas:
 // Implementa pegar Nome e ID de maneira mais fácil
 // Implementa automatizar utilizando algum método como Onload para não precisar presisonar F2
+
+// Motivos para instalar a extensão:
+// Auxilia a ignorar usuários
+// Auxiliar a ter paciência com usuários
+// Filtra usuários que fazem spam, não respondem comentários, robôs, renunciam a lógica
+// Aumenta a escala de contribuições conscientes aos debates
