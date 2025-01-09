ac723d32d81681113fafbe09539cfb4a88923454
Joao Vitor Nogueira da Silva <joao.nogueira@digisystem.com.br>
2025-01-09 19:32:06 -0300

    Implementa README

---
+++ README.md
@@ -0,0 +1,17 @@
+# Tribunal da Ação Humana
+
+**Tribunal da Ação Humana** é uma extensão voltada para abordar questões de lógica aplicadas à economia, filosofia, ciência política e direito. O projeto busca sinalizar em vermelho o nome dos usuários do Facebook que renunciaram a lógica. O banco de dados tem atualização recorrente conforme feedback.
+
+## Funcionalidades
+
+- Aumento da escala de contribuições conscientes aos debates
+- Redução de spam, troll e flood
+- Auxilio para ignorar ou ter paciência com usuários
+
+## Instalação
+
+- Em breve a extensão vai ser disponibilizada gratuitamente na loja do navegador Edge e então o link informado aqui.
+
+## Autor
+
+[João Vitor Nogueira](https://www.linkedin.com/in/nogueira-jv/)
--- temp.js
+++ temp.js
@@ -3,7 +3,14 @@
 // Implementa automatizar utilizando algum método como Onload para não precisar presisonar F2
 
 // Motivos para instalar a extensão:
+// Evita spam, troll e flood
+// Aumenta a escala de contribuições conscientes aos debates
 // Auxilia a ignorar usuários
 // Auxiliar a ter paciência com usuários
-// Filtra usuários que fazem spam, não respondem comentários, robôs, renunciam a lógica
-// Aumenta a escala de contribuições conscientes aos debates
+
+// Pensar na proporcionalidade das punições:
+// Pensar que se assumir o erro, poderá ter a revogação
+// Pensar que devido a recorrência, vai ter a punição proporcional
+
+// Público alvo que vai ser afetado:
+// Assuntos de economia, filosofia, ciência política e direito
