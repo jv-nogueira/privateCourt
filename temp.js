7ea40759237b24355566f7817d7d0408c89ed577
Joao Vitor Nogueira da Silva <joao.nogueira@digisystem.com.br>
2025-01-18 18:05:06 -0300

    Testa setar a cor no feed principal também

--- temp.js
+++ temp.js
@@ -1,16 +1,9 @@
 // Próximas etapas:
 // Implementa pegar Nome e ID de maneira mais fácil
-// Implementa automatizar utilizando algum método como Onload para não precisar presisonar F2
 
-// Motivos para instalar a extensão:
-// Evita spam, troll e flood
-// Aumenta a escala de contribuições conscientes aos debates
-// Auxilia a ignorar usuários
-// Auxiliar a ter paciência com usuários
+// No feed, setar a cor pelo nome da pessoa
+document.querySelectorAll("[data-pagelet='FeedUnit_{n}']")[i].children[0].children[0].children[0].children[0].children[0].children[0].children[0].children[0].children[0].children[0].children[0].children[0].children[0].children[1].children[0].children[0].children[1].children[0].children[1].children[0].children[1].children[0].children[0].children[0].children[0].children[0].children[0].children[0]
 
-// Pensar na proporcionalidade das punições:
-// Pensar que se assumir o erro, poderá ter a revogação
-// Pensar que devido a recorrência, vai ter a punição proporcional
+// Ou tentar usar esse
+document.querySelectorAll("[data-ad-rendering-role='profile_name']")[6].parentElement.parentElement.parentElement.children[1].children[0].children[0].children[0].children[0].children[0].children[0].children[0]
 
-// Público alvo que vai ser afetado:
-// Assuntos de economia, filosofia, ciência política e direito
